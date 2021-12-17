7cc7f6056c258a97e72de65e5016b8a4dbe67d48
deepakphpweb <dipakrana2760@gmail.com>
2021-12-17 23:38:09 +0530

    Added : Welcome to Mood Anayzer Problem

---
+++ src/main/java/com/bridge/exception/MoodAnalyzerException.java
@@ -0,0 +1,7 @@
+package com.bridge.exception;
+
+public class MoodAnalyzerException {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Mood Analyzer Problem");
+    }
+}
